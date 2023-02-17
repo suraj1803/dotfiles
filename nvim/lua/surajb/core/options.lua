464938adc90021d9f79bc5e7feee9ea562e80966
suraj1803 <surajbiswas182003@gmail.com>
2023-02-18 01:09:46 +0530

    Update options.lua

--- nvim/lua/surajb/core/options.lua
+++ nvim/lua/surajb/core/options.lua
@@ -18,7 +18,7 @@ opt.ignorecase = true -- ignore case when searching
 opt.smartcase = true -- if you include mixed case in your search, assumes you want case-sensitive
 
 -- cursor line
-opt.cursorline = true -- highlight the current cursor line
+-- opt.cursorline = true -- highlight the current cursor line
 
 -- appearance
 
