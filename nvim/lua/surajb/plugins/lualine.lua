c92bf60516aca3482ba95d839c0c5182566299e4
suraj1803 <surajbiswas182003@gmail.com>
2023-02-18 01:10:15 +0530

    Update lualine.lua

--- nvim/lua/surajb/plugins/lualine.lua
+++ nvim/lua/surajb/plugins/lualine.lua
@@ -5,7 +5,7 @@ if not status then
 end
 
 -- get lualine nightfly theme
-local lualine_nightfly = require("lualine.themes.nightfly")
+local lualine_nightfly = require("lualine.themes.solarized_dark")
 
 -- new colors for theme
 local new_colors = {
