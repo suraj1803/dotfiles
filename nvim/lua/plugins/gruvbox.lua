d417a57c554938babccc026ba6951af7f83645c1
Suraj Biswas <surajbiswas182003@gmail.com>
2024-05-17 10:58:42 +0530

    Delete nvim directory

--- nvim/lua/plugins/gruvbox.lua
+++
@@ -1,9 +0,0 @@
-return {
-    "ellisonleao/gruvbox.nvim", priority = 1000 , config = true, opts = ...,
-	config = function()
-		vim.cmd.colorscheme("gruvbox")
-		vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-
-		vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
-	end,
-}
