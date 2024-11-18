547620453d06c53d204db5f2f2813dc189066c94
Suraj Biswas <surajbiswas182003@gmail.com>
2024-11-18 21:51:27 +0530

    added nvim config

---
+++ nvim/lua/config/core.lua
@@ -0,0 +1,79 @@
+vim.cmd("let g:netrw_liststyle = 3")
+vim.api.nvim_create_autocmd("VimEnter", {
+  callback = function()
+    vim.opt.laststatus = 3
+  end,
+})
+
+
+local opt = vim.opt
+
+opt.relativenumber = true
+opt.number = true
+
+-- tabs & indentation
+opt.tabstop = 2 -- 2 spaces for tabs (prettier default)
+opt.shiftwidth = 2 -- 2 spaces for indent width
+opt.expandtab = true -- expand tab to spaces
+opt.autoindent = true -- copy indent from current line when starting new one
+
+opt.wrap = false
+
+-- search settings
+opt.ignorecase = true -- ignore case when searching
+opt.smartcase = true -- if you include mixed case in your search, assumes you want case-sensitive
+
+opt.cursorline = true
+
+-- turn on termguicolors for tokyonight colorscheme to work
+-- (have to use iterm2 or any other true color terminal)
+opt.termguicolors = true
+opt.background = "dark" -- colorschemes that can be light or dark will be made dark
+opt.signcolumn = "yes" -- show sign column so that text doesn't shift
+
+-- backspace
+opt.backspace = "indent,eol,start" -- allow backspace on indent, end of line or insert mode start position
+
+-- clipboard
+opt.clipboard:append("unnamedplus") -- use system clipboard as default register
+
+-- split windows
+opt.splitright = true -- split vertical window to the right
+opt.splitbelow = true -- split horizontal window to the bottom
+
+-- turn off swapfile
+opt.swapfile = false
+
+
+-- Key Mappings 
+vim.g.mapleader = " "
+
+local keymap = vim.keymap -- for conciseness
+
+keymap.set("i", "jj", "<ESC>", { desc = "Exit insert mode with jk" })
+
+keymap.set("n", "<leader>nh", ":nohl<CR>", { desc = "Clear search highlights" })
+
+-- increment/decrement numbers
+keymap.set("n", "<leader>+", "<C-a>", { desc = "Increment number" }) -- increment
+keymap.set("n", "<leader>-", "<C-x>", { desc = "Decrement number" }) -- decrement
+
+-- window management
+keymap.set("n", "<leader>sv", "<C-w>v", { desc = "Split window vertically" }) -- split window vertically
+keymap.set("n", "<leader>sh", "<C-w>s", { desc = "Split window horizontally" }) -- split window horizontally
+keymap.set("n", "<leader>se", "<C-w>=", { desc = "Make splits equal size" }) -- make split windows equal width & height
+keymap.set("n", "<leader>sx", "<cmd>close<CR>", { desc = "Close current split" }) -- close current split window
+
+keymap.set("n", "<leader>to", "<cmd>tabnew<CR>", { desc = "Open new tab" }) -- open new tab
+keymap.set("n", "<leader>tx", "<cmd>tabclose<CR>", { desc = "Close current tab" }) -- close current tab
+keymap.set("n", "<leader>tn", "<cmd>tabn<CR>", { desc = "Go to next tab" }) --  go to next tab
+keymap.set("n", "<leader>tp", "<cmd>tabp<CR>", { desc = "Go to previous tab" }) --  go to previous tab
+keymap.set("n", "<leader>tf", "<cmd>tabnew %<CR>", { desc = "Open current buffer in new tab" }) --  move current buffer to new tab
+
+
+-- terminal split
+vim.api.nvim_set_keymap('n', '<leader>ts', ':split | terminal<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>tv', ':vsplit | terminal<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('t', '<C-n>', [[<C-\><C-n>]], { noremap = true, silent = true })
+
+
---
+++ nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,72 @@
+--   return { "catppuccin/nvim", name = "catppuccin", priority = 1000,
+--   config =function()
+--     require("catppuccin").setup({
+--       flavour = "mocha", -- latte, frappe, macchiato, mocha
+--       background = { -- :h background
+--         light = "latte",
+--         dark = "mocha",
+--       },
+--       transparent_background = true, -- disables setting the background color.
+--       show_end_of_buffer = false, -- shows the '~' characters after the end of buffers
+--       term_colors = false, -- sets terminal colors (e.g. `g:terminal_color_0`)
+--       dim_inactive = {
+--         enabled = false, -- dims the background color of inactive window
+--         shade = "dark",
+--         percentage = 0.15, -- percentage of the shade to apply to the inactive window
+--       },
+--       no_italic = false, -- Force no italic
+--       no_bold = false, -- Force no bold
+--       no_underline = false, -- Force no underline
+--       styles = { -- Handles the styles of general hi groups (see `:h highlight-args`):
+--         comments = { "italic" }, -- Change the style of comments
+--         conditionals = { "italic" },
+--         loops = {},
+--         functions = {"bold"},
+--         keywords = {"bold"},
+--         strings = {},
+--         variables = {},
+--         numbers = {},
+--         booleans = {},
+--         properties = {},
+--         types = {},
+--         operators = {},
+--         -- miscs = {}, -- Uncomment to turn off hard-coded styles
+--       },
+--       color_overrides = {},
+--       custom_highlights = {},
+--       default_integrations = true,
+--       integrations = {
+--         cmp = true,
+--         gitsigns = true,
+--         nvimtree = true,
+--         treesitter = true,
+--         notify = false,
+--         mini = {
+--           enabled = true,
+--           indentscope_color = "",
+--         },
+--         -- For more plugins integrations please scroll down (https://github.com/catppuccin/nvim#integrations)
+--       },
+--     })
+--
+--     -- setup must be called before loading
+--     vim.cmd.colorscheme "catppuccin"
+--   end
+-- }
+
+
+return  {
+  'sainnhe/gruvbox-material',
+  lazy = false,
+  priority = 1000,
+  config = function()
+    -- vim.g.gruvbox_material_enable_italic = true
+    vim.g.gruvbox_material_enable_bold = true
+    vim.g.gruvbox_material_background = 'hard'
+    vim.g.gruvbox_material_foreground = 'original'
+    vim.g.gruvbox_material_transparent_background = 1
+    vim.cmd.colorscheme('gruvbox-material')
+    vim.o.background = "dark" -- or "light" for light mode
+  end
+}
+
