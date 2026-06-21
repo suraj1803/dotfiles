5f5b0095c64d4209c4885ebeaec0c4b58a64c1cd
Suraj Biswas <surajbiswas182003@gmail.com>
2026-06-21 19:10:54 +0530

    added

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -17,28 +17,41 @@
 -- 	end,
 -- }
 
-return {
-	"rebelot/kanagawa.nvim",
-	config = function()
-		-- Default options:
-		require("kanagawa").setup({
-			compile = false, -- enable compiling the colorscheme
-			undercurl = true, -- enable undercurls
-			commentStyle = { italic = true },
-			functionStyle = {},
-			keywordStyle = { italic = true },
-			statementStyle = { bold = true },
-			typeStyle = {},
-			colors = {
-				theme = { all = { ui = { bg_gutter = "none" } } },
-			},
-			transparent = true, -- do not set background color
-			dimInactive = false, -- dim inactive window `:h hl-NormalNC`
-			terminalColors = true, -- define vim.g.terminal_color_{0,17}
-			theme = "dragon", -- Load "wave" theme
-		})
+-- return {
+-- 	"rebelot/kanagawa.nvim",
+-- 	config = function()
+-- 		-- Default options:
+-- 		require("kanagawa").setup({
+-- 			compile = false, -- enable compiling the colorscheme
+-- 			undercurl = true, -- enable undercurls
+-- 			commentStyle = { italic = true },
+-- 			functionStyle = {},
+-- 			keywordStyle = { italic = true },
+-- 			statementStyle = { bold = true },
+-- 			typeStyle = {},
+-- 			colors = {
+-- 				theme = { all = { ui = { bg_gutter = "none" } } },
+-- 			},
+-- 			transparent = true, -- do not set background color
+-- 			dimInactive = false, -- dim inactive window `:h hl-NormalNC`
+-- 			terminalColors = true, -- define vim.g.terminal_color_{0,17}
+-- 			theme = "dragon", -- Load "wave" theme
+-- 		})
+--
+-- 		-- setup must be called before loading
+-- 		vim.cmd("colorscheme kanagawa")
+-- 	end,
+-- }
 
-		-- setup must be called before loading
-		vim.cmd("colorscheme kanagawa")
+return {
+	"bluz71/vim-moonfly-colors",
+	name = "moonfly",
+	lazy = false,
+	priority = 1000,
+	config = function()
+		-- Lua initialization file
+		vim.g.moonflyTransparent = true
+		-- Lua initialization file
+		vim.cmd([[colorscheme moonfly]])
 	end,
 }
