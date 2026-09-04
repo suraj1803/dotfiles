eaaaf3b58243037aa3193c96d81ac91acbe6def6
Suraj Biswas <surajbiswas182003@gmail.com>
2026-09-04 20:53:23 +0530

    changed

--- nvim/.config/nvim/lua/config/general.lua
+++ nvim/.config/nvim/lua/config/general.lua
@@ -22,7 +22,7 @@ opt.cursorline = true
 -- turn on termguicolors for tokyonight colorscheme to work
 -- (have to use iterm2 or any other true color terminal)
 opt.termguicolors = true
-opt.background = "dark" -- colorschemes that can be light or dark will be made dark
+-- opt.background = "dark" -- colorschemes that can be light or dark will be made dark
 opt.signcolumn = "yes" -- show sign column so that text doesn't shift
 
 -- backspace
