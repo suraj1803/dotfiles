dc5d4457c78bcc3920490365531e93162f542fba
Suraj Biswas <surajbiswas182003@gmail.com>
2026-09-14 21:53:51 +0530

    modified

--- nvim/.config/nvim/lua/plugins/general.lua
+++ nvim/.config/nvim/lua/plugins/general.lua
@@ -1,35 +1,30 @@
 return {
+    -- Using Lazy
     {
-        "rebelot/kanagawa.nvim",
-        config = function()
-            -- Default options:
-            require('kanagawa').setup({
-                compile = false,             -- enable compiling the colorscheme
-                undercurl = true,            -- enable undercurls
-                commentStyle = { italic = true },
-                functionStyle = {},
-                keywordStyle = { italic = true},
-                statementStyle = { bold = true },
-                typeStyle = {},
-                transparent = true,         -- do not set background color
-                dimInactive = false,         -- dim inactive window `:h hl-NormalNC`
-                terminalColors = true,       -- define vim.g.terminal_color_{0,17}
-                colors = {                   -- add/modify theme and palette colors
-                    palette = {},
-                    theme = { wave = {}, lotus = {}, dragon = {}, all = {ui = {bg_gutter="none"}, float = {bg="none"}  }},
+        "navarasu/onedark.nvim",
+        priority = 1000, -- make sure to load this before all the other start plugins
+        config = function() 
+            require('onedark').setup {
+                style = "darker",
+                transparent = true,
+                code_style = {
+                    comments = 'none',
+                    keywords = 'bold',
+                    functions = 'bold',
+                    strings = 'none',
+                    variables = 'none'
                 },
-                overrides = function(colors) -- add/modify highlights
-                    return {}
-                end,
-                theme = "wave",              -- Load "wave" theme
-                background = {               -- map the value of 'background' option to a theme
-                    dark = "wave",           -- try "dragon" !
-                    light = "lotus"
-                },
-            })
 
-            -- setup must be called before loading
-            vim.cmd("colorscheme kanagawa")
+            }
+            -- Lua
+            require('onedark').load()
+        end
+    },
+    {
+        'nvim-mini/mini.statusline',
+        version = false,
+        config = function ()
+            require('mini.statusline').setup()
         end
     },
 
